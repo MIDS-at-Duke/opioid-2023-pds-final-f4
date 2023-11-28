19e5afa9b13cde4bfe9f7d7276f1194d139fa60e
yourdocxfile <19l207@psgtech.ac.in>
2023-11-27 19:46:15 -0500

    made the pre-post and DiD plots for vital statistics data

--- 10_code/merge_pop_death.py
+++ 10_code/merge_pop_death.py
@@ -2,8 +2,8 @@ import pandas as pd
 import numpy as np
 
 # Specify the file paths
-file_path_population = r'C:\Users\19l20\Desktop\mids_study\pds_final_project\population.csv'
-file_path_vs_dataset = r'C:\Users\19l20\Desktop\mids_study\pds_final_project\vs_dataset.csv'
+file_path_population = r'C:\\Users\\19l20\\Desktop\\mids_study\\pds_final_project\\opioid-2023-pds-final-f4\\20_intermediate_files\\population.csv'
+file_path_vs_dataset = r'C:\\Users\\19l20\\Desktop\\mids_study\\pds_final_project\\opioid-2023-pds-final-f4\\20_intermediate_files\\vs_dataset.csv'
 
 # Read the CSV files into pandas DataFrames
 df_population = pd.read_csv(file_path_population)
@@ -12,12 +12,13 @@ df_vs_dataset = pd.read_csv(file_path_vs_dataset)
 # Merge the two columns and create a new column named "County_Code" in the population DataFrame
 df_population['County_Code'] = df_population['STUSAB'] + '-' + df_population['County Code'].astype(str)
 
-print("the population code is", df_population.head())
+df_population = df_population[df_population['_merge'] == 'both']
+
 
 # Merge the two datasets on the "County_Code" column
 merged_df = pd.merge(df_population, df_vs_dataset, on="County_Code", how="inner")
 
-print("the merged dataframe is:", merged_df.head())
+# print("the merged dataframe is:", merged_df.head())
 
 # Initialize a list of columns for the final DataFrame
 final_columns = ['County_Code']
@@ -36,10 +37,8 @@ for year in years:
     merged_df[population_column] = pd.to_numeric(merged_df[population_column], errors='coerce').astype(float)
     
     # Calculate deaths per capita, handling potential division by zero
-    merged_df[deaths_per_capita_column] = (
-        merged_df[death_column] / merged_df[population_column]
-    ).replace([np.inf, -np.inf], np.nan).fillna(0)
-
+    merged_df[deaths_per_capita_column] = np.where(merged_df[population_column] != 0, merged_df[death_column] / merged_df[population_column], 0)
+    
     # Add the deaths per capita to the final DataFrame
     final_columns.append(deaths_per_capita_column)
 
@@ -50,5 +49,5 @@ final_df = merged_df[final_columns]
 print(final_df.head())
 
 
-# If you want to save the final dataset to a new CSV file, you can use the following line
-final_df.to_csv(r'C:\Users\19l20\Desktop\mids_study\pds_final_project\final_dataset.csv', index=False)
+
+
